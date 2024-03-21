f080f0b041edcba73cf6095e9bdf31f702738347
AerhonJames <aerhon.james@gmail.com>
2024-03-21 12:55:59 +0800

    Updated: webpack.config.js to set dev server to have a live reload

---
+++ src/assets/js/theme.js
@@ -0,0 +1,2 @@
+document.querySelector("h1").innerText = 'asdsd';
+console.log(21001100);
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,71 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+// paths used in various placed in webpack config
+const paths = {
+	src: {
+		imgs: './src/assets/images',
+		scss: './src/assets/scss',
+		fonts: './src/assets/fonts',
+		js: './src/assets/js',
+		favicon: './src/assets/favicon',
+	},
+	dist: {
+		imgs: './assets/images',
+		css: './assets/css',
+		fonts: './assets/fonts',
+		js: './assets/js',
+		favicon: './assets/favicon',
+	}
+}
 
 const wPackConfig = {
-	entry: {
-		theme: path.resolve(__dirname, 'src/assets/js/theme.js')
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: '[name].js'
-	},
-  mode: 'development'
+  entry: {
+    libs: [paths.src.scss + '/libs.scss'],
+    theme: [paths.src.js + '/theme.js', paths.src.scss + '/theme.scss']
+  },
+  output: {
+    filename: paths.dist.js + '/[name].bundle.js'
+  },
+  target: 'web',
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'src/html')
+    },
+    port: 3000,
+    open: true,
+    watchFiles: ['src/**/*'],
+  },
+	mode: 'development',
+  module: {
+    rules: [{
+      test: /\.(sass|scss|css)$/,
+      include: path.resolve(__dirname, paths.src.scss.slice(2)),
+      use: [
+        {
+        loader: MiniCssExtractPlugin.loader,
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            url: false
+          }
+        },
+        {
+          loader: 'postcss-loader',
+        },
+        {
+          loader: 'sass-loader'
+        }
+    ]
+    }]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: paths.dist.css + '/[name].bundle.css'
+    })
+  ]
 };
 
 module.exports = wPackConfig;
