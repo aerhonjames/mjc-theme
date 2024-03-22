622c59d6e49c290d15e15839b47ef84e80a58800
AerhonJames <aerhon.james@gmail.com>
2024-03-22 15:47:05 +0800

    Added: fonts, components for header

---
+++ src/assets/js/components/navbar.js
@@ -0,0 +1,41 @@
+(function(){
+    const megaMenuToggles = document.querySelectorAll('.nav-item.dropdown');
+    const cssVarBreakpoint = getComputedStyle(document.documentElement).getPropertyValue('--theme-breakpoint-lg') || '992px';
+    const breakpointLG = parseInt(cssVarBreakpoint);
+    
+    function showMenu(event){
+        const dropdown = event.target.querySelector('.dropdown-megamenu');
+        const menuToggle = event.target.querySelector('.dropdown-toggle');
+
+        if(window.innerWidth < breakpointLG) return;
+
+        if(dropdown && menuToggle){
+            dropdown.classList.add('show');
+            menuToggle.setAttribute('aria-expanded', 'true');
+            menuToggle.classList.add('show');
+        }
+    }
+
+    function hideMenu(event){
+        const dropdown = event.target.querySelector('.dropdown-megamenu');
+        const menuToggle = event.target.querySelector('.dropdown-toggle');
+
+        if(window.innerWidth < breakpointLG) return;
+        
+        if(dropdown && menuToggle){
+            dropdown.classList.remove('show');
+            menuToggle.setAttribute('aria-expanded', 'false');
+            menuToggle.classList.remove('show');
+        }
+    }
+
+    megaMenuToggles.forEach(function(toggle){
+        toggle.addEventListener('mouseenter', (event) => {
+            showMenu(event);
+        });
+
+        toggle.addEventListener('mouseleave', (event) => {
+            hideMenu(event);
+        });
+    });
+})();
